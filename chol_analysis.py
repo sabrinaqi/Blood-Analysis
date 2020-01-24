3cc594b6f33ab1e0d5cc50990d05dedbaf91412d
sabrinaqi <sabrina.qi@duke.edu>
2020-01-24 12:34:32 -0500

    cholesterol data input parser added

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,11 +1,20 @@
+def cholesterol_analysis():
+	print("Cholesterol Analysis")
+	HDLinput = input("Enter test result: ")
+	test_info = HDLinput.split("=")
+	if test_info[0] == "HDL":
+    	HDL_analysis():
 def interface():
-	while True:
+    while True:
 		print("Cholesterol Calculator")
 		print("Options: ")
+		print(" 1 - Cholesterol Analysis")
 		print(" 9 - Quit")
 		choice = input("Enter your option: ")
 		if choice == "9":
 			return
+		elif choice == "1":
+    			cholesterol_analysis()
 
 if __name__ == "__main__":
 	interface()
