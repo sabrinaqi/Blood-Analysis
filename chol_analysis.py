bd137aa601caf67ca30da90e5a39da71db7327e1
sabrinaqi <sabrina.qi@duke.edu>
2020-01-25 14:02:50 -0500

    added ldl analysis

--- chol_analysis.py
+++ chol_analysis.py
@@ -7,13 +7,29 @@ def HDL_analysis(HDL_level):
 		else:
 				return "Low"
 
+def LDL_analysis(LDL_level):
+						print(LDL_level)
+						if LDL_level < 130:
+								return "Normal"
+						elif 130 <= LDL_level < 159:
+								return "Borderline high"
+						elif 160 <= LDL_level < 189:
+								return "High"
+						else:
+								return "Very high"
+
 def cholesterol_analysis():
 		print("Cholesterol Analysis")
-		HDLinput = input("Enter test result: ")
-		test_info = HDLinput.split("=")
+		input_val = input("Enter test result: ")
+		test_info = input_val.split("=")
 		if test_info[0] == "HDL":
 				answer = HDL_analysis(int(test_info[1]))
-				print("The level is {}".format(answer))
+				print("The {} level is {}".format(test_info[0],answer))
+		elif test_info[0] == "LDL":
+				answer = LDL_analysis(int(test_info[1]))
+				print("The {} level is {}".format(test_info[0],answer))
+		else:
+    			print("We do not test for this")
 
 def interface():
 		while True:
